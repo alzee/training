866de199dccea3282de4d23a898b617f3f0b38b3
Al Zee <z@alz.ee>
2021-03-10 10:04:20 +0800

    nth

--- src/Controller/Admin/TraineeCrudController.php
+++ src/Controller/Admin/TraineeCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Trainee;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class TraineeCrudController extends AbstractCrudController
 {
@@ -22,4 +25,12 @@ class TraineeCrudController extends AbstractCrudController
         ];
     }
     */
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
+        ;
+    }
 }
