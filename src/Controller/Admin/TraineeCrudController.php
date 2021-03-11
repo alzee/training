7275c5c588f07cc9969f866e8113955e1b72edb4
Al Zee <z@alz.ee>
2021-03-11 11:36:13 +0800

    filters for trainee

--- src/Controller/Admin/TraineeCrudController.php
+++ src/Controller/Admin/TraineeCrudController.php
@@ -7,6 +7,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ArrayFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
@@ -17,6 +20,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class TraineeCrudController extends AbstractCrudController
 {
+    private $skills = ['急救护理' => 0,'特殊装备操作' => 1, '机动车驾驶' => 2];
+    private $pstatus = ['厨师' => 0,'司机' => 1, '老师' => 2];
+    private $politics = ['群众' => 0,'党员' => 1, '军人' => 2];
+    private $sex = ['男' => 0,'女' => 1];
+
     public static function getEntityFqcn(): string
     {
         return Trainee::class;
@@ -28,14 +36,14 @@ class TraineeCrudController extends AbstractCrudController
             IdField::new('id')->onlyOnIndex(),
             TextField::new('name'),
             IntegerField::new('age'),
-            ChoiceField::new('sex')->setChoices(['男' => 0,'女' => 1]),
-            ChoiceField::new('pstatus')->setChoices(['厨师' => 0,'司机' => 1, '老师' => 2]),
-            ChoiceField::new('politics')->setChoices(['群众' => 0,'党员' => 1, '军人' => 2]),
+            ChoiceField::new('sex')->setChoices($this->sex),
+            ChoiceField::new('pstatus')->setChoices($this->pstatus),
+            ChoiceField::new('politics')->setChoices($this->politics),
             TextField::new('area'),
             TextField::new('phone'),
             TextField::new('address'),
             TextField::new('idnum'),
-            ChoiceField::new('skill')->setChoices(['急救护理' => 0,'特殊装备操作' => 1, '机动车驾驶' => 2]),
+            ChoiceField::new('skill')->setChoices($this->skills),
         ];
     }
 
@@ -45,6 +53,17 @@ class TraineeCrudController extends AbstractCrudController
         return $actions
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
             ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('area')
+            ->add('age')
+            ->add(ChoiceFilter::new('pstatus')->setChoices($this->pstatus))
+            ->add(ChoiceFilter::new('politics')->setChoices($this->politics))
+            ->add(ChoiceFilter::new('skill')->setChoices($this->skills))
         ;
     }
 }
