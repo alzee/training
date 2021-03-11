f0b905e3a049e76285620c2e9cb5ca69dcf5689a
Al Zee <z@alz.ee>
2021-03-12 03:39:45 +0800

    chart1/chart2 done

--- public/js/initChart.js
+++ public/js/initChart.js
@@ -3,19 +3,35 @@ let ctx2 = document.getElementById('chart2');
 let ctx3 = document.getElementById('chart3');
 let ctx4 = document.getElementById('chart4');
 let ctx5 = document.getElementById('chart5');
-
-let data = {
-    labels: ['1月', '2月', '3月', '4月', '5月', '6月'],
-    datasets: [{
-        label: '物品领用统计',
-        barPercentage: 0.5,
-        barThickness: 6,
-        maxBarThickness: 8,
-        minBarLength: 2,
-        backgroundColor: '#0d6efd',
-        data: [10, 20, 30, 40, 50, 60, 70]
-    }]
+let d1 = document.querySelectorAll('.areaPeople');
+let data1 = {
+  labels: [],
+  datasets: [{
+    label: '领用排行榜',
+    backgroundColor: '#0dcaf0',
+    data: []
+  }]
 };
+for (let i = 0; i < d1.length; i++){
+  data1.labels.push((d1[i].dataset.area));
+  data1.datasets[0].data.push((d1[i].dataset.people));
+  console.log(d1[i].dataset.area);
+}
+
+let d2 = document.querySelectorAll('.ageGroup');
+let data2 = {
+  labels: [],
+  datasets: [{
+    backgroundColor: ['#0dcaf0', "rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)", "#cd3c63", '#6174d1'],
+    data: []
+  }]
+};
+for (let i = 0; i < d2.length; i++){
+  console.log(d2[i]);
+  data2.labels.push((d2[i].dataset.age));
+  data2.datasets[0].data.push((d2[i].dataset.people));
+  console.log(d2[i].dataset.age);
+}
 
 let options = {
     responsive:true,
@@ -34,28 +50,13 @@ let options = {
 
 let chart1 = new Chart(ctx1, {
     type: 'horizontalBar',
-    //data: data,
-    data: {
-        labels: ['1月', '2月', '3月', '4月', '5月', '6月'],
-        datasets: [{
-            label: '领用排行榜',
-            backgroundColor: '#0dcaf0',
-            data: [638, 300, 488, 1050, 214, 843]
-        }]
-    },
+    data: data1,
     options: options
 });
 
 let chart2 = new Chart(ctx2, {
     type: 'pie',
-    //data: data,
-    data: {
-        labels: ['20-25', '25-30', '30-35', '35-40', '40-45', '18-20'],
-        datasets: [{
-            backgroundColor: ['#0dcaf0', "rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)", "#cd3c63", '#6174d1'],
-            data: [638, 300, 488, 1050, 214, 843]
-        }]
-    },
+    data: data2,
     options: {
       legend: {
         display: false,
