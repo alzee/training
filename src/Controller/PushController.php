c6a7039a5bc3fd4895b7725a9db73cdce4427c4b
Al Zee <z@alz.ee>
2021-03-14 07:47:35 +0800

    PushController

---
+++ src/Controller/PushController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PushController extends AbstractController
+{
+    /**
+     * @Route("/push", name="push")
+     */
+    public function index(): Response
+    {
+// 建立socket连接到内部推送端口
+
+$client = stream_socket_client('tcp://127.0.0.1:5678', $errno, $errmsg, 1);
+
+// 推送的数据，包含uid字段，表示是给这个uid推送
+
+$data = array('uid'=>'1', 'percent'=>'88%');
+
+// 发送数据，注意5678端口是Text协议的端口，Text协议需要在数据末尾加上换行符
+
+fwrite($client, json_encode($data)."\n");
+
+// 读取推送结果
+
+echo fread($client, 8192);
+
+  
+        return $this->json($data);
+    }
+}
