3ec87dbe6fec098c540f8126e954c29defd3ebbb
Al Zee <z@alz.ee>
2021-03-14 08:53:09 +0800

    declare and ping ok

--- src/Controller/PushController.php
+++ src/Controller/PushController.php
@@ -13,6 +13,7 @@ class PushController extends AbstractController
      */
     public function index(): Response
     {
+        /*
         // 建立socket连接到内部推送端口
 
         $client = stream_socket_client('tcp://127.0.0.1:8001', $errno, $errmsg, 3);
@@ -28,8 +29,14 @@ class PushController extends AbstractController
         // 读取推送结果
 
         echo fread($client, 8192);
+         */
+
+        $client = stream_socket_client('tcp://127.0.0.1:7273');
+        if(!$client)exit("can not connect");
+        // 模拟超级用户，以文本协议发送数据，注意Text文本协议末尾有换行符（发送的数据中最好有能识别超级用户的字段），这样在Event.php中的onMessage方法中便能收到这个数据，然后做相应的处理即可
+        fwrite($client, '{"cmd":"to_device","to":"7f0000010b5600000001","type":"send","content":"hello all", "user":"admin", "pass":"******"}'."\n");
 
 
-        return $this->json($data);
+        return $this->json(1);
     }
 }
