84274f735a2e862de1ba4f0b6e61a6b60d3d27b9
Al Zee <z@alz.ee>
2021-03-14 09:06:54 +0800

    finally we use gatewayclient

--- src/Controller/PushController.php
+++ src/Controller/PushController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use GatewayClient\Gateway;
 
 class PushController extends AbstractController
 {
@@ -13,30 +14,12 @@ class PushController extends AbstractController
      */
     public function index(): Response
     {
-        /*
-        // 建立socket连接到内部推送端口
+        //Gateway::bindUid($client_id, 设备id);
+        //Gateway::sendToUid(设备id, 数据);
+        Gateway::$registerAddress = '127.0.0.1:1238';
+        $data = '{"cmd":"to_device","to":"7f0000010b5600000001","type":"send","content":"hello all", "user":"admin", "pass":"******"}';
+        Gateway::sendToAll($data);
 
-        $client = stream_socket_client('tcp://127.0.0.1:8001', $errno, $errmsg, 3);
-
-        // 推送的数据，包含uid字段，表示是给这个uid推送
-
-        $data = array('uid'=>'1', 'percent'=>'88%');
-
-        // 发送数据，注意5678端口是Text协议的端口，Text协议需要在数据末尾加上换行符
-
-        fwrite($client, json_encode($data)."\n");
-
-        // 读取推送结果
-
-        echo fread($client, 8192);
-         */
-
-        $client = stream_socket_client('tcp://127.0.0.1:7273');
-        if(!$client)exit("can not connect");
-        // 模拟超级用户，以文本协议发送数据，注意Text文本协议末尾有换行符（发送的数据中最好有能识别超级用户的字段），这样在Event.php中的onMessage方法中便能收到这个数据，然后做相应的处理即可
-        fwrite($client, '{"cmd":"to_device","to":"7f0000010b5600000001","type":"send","content":"hello all", "user":"admin", "pass":"******"}'."\n");
-
-
-        return $this->json(1);
+        return $this->json(0);
     }
 }
