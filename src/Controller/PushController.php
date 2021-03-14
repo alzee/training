e052f547e5a9e8d2121cdf4b4e8fedc0e2389f66
Al Zee <z@alz.ee>
2021-03-14 16:00:22 +0800

    nth

--- src/Controller/PushController.php
+++ src/Controller/PushController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use GatewayClient\Gateway;
 
+/** 
+ * websocket 接口文档 https://www.showdoc.com.cn/286682190140856
+ */
 class PushController extends AbstractController
 {
     /**
@@ -18,15 +21,18 @@ class PushController extends AbstractController
         //Gateway::sendToUid(设备id, 数据);
         Gateway::$registerAddress = '127.0.0.1:8001';
         $data = [
-            "cmd" => "onlineAuthorization",
-            //"cmd" => "addUser",
-            //"user_id" => 800003,
-            //"name" => '杨兴伟',
-            //"id_card" => '420302199012121111',
-            //"id_valid" => '',
-            //"Ic" => '111',
-            //"face_template" => 'https://uploadfile.bizhizu.cn/up/d2/7f/15/d27f157652629be2d04f41e7c697c25a.jpg',
-            //"vlface_template" => ''
+            "cmd" => "addUser",
+            //"cmd" => "onlineAuthorization",
+            //"cmd" => "editUser",
+            //"cmd" => "delUser",
+            //"cmd" => "delMultiUserRet",
+            //"cmd" => "delAllUser",
+            "user_id" => 800003,
+            "name" => '杨',
+            "id_card" => '420302199012121111',
+            "id_valid" => '',
+            // 验证模式为人脸或卡时照片才不是非必填，但此模式下 Ic 必填
+            "Ic" => '111',
         ];
         $resp = [
             "cmd" => "to_device",
