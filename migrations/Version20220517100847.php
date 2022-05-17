08443170315ac356d916318c53a1d2160485225c
Al Zee <z@alz.ee>
2022-05-17 22:21:36 +0800

    nth

--- migrations/Version20220517100847.php
+++ migrations/Version20220517100847.php
@@ -20,7 +20,7 @@ final class Version20220517100847 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE trainee ADD face TINYINT(1) DEFAULT NULL, DROP test');
+        $this->addSql('ALTER TABLE trainee ADD face TINYINT(1) DEFAULT NULL, DROP if exists test');
     }
 
     public function down(Schema $schema): void
