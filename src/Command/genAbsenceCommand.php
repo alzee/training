6fa014ae69d287cd4a4bda73aff1eac7f62f1e95
Al Zee <z@alz.ee>
2025-03-16 23:05:42 +0800

    nth

---
+++ src/Command/genAbsenceCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Absence;
+
+class GenAbsenceCommand extends Command
+{
+    protected static $defaultName = 'app:gen-absence';
+    protected static $defaultDescription = 'gen absence records';
+
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription(self::$defaultDescription)
+            // ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            // ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        // $arg1 = $input->getArgument('arg1');
+
+        
+        $this->em->flush();
+
+        $io->success('Done.');
+
+        return Command::SUCCESS;
+    }
+}
